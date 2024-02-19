b8b00a30758552b6a6bed4442eb81299183a4d21
ritesh1617 <133222476+ritesh1617@users.noreply.github.com>
2024-02-19 12:17:35 +0530

    1

--- vijay_xsquare-zevance-2374aae401a1/vijay_xsquare-zevance-2374aae401a1/lib/model/FileData.dart
+++
@@ -1,9 +0,0 @@
-
-import 'dart:io';
-
-class FileData {
-  String type;
-  File file;
-
-  FileData(this.type,this.file);
-}
--- vijay_xsquare-zevance-2374aae401a1/vijay_xsquare-zevance-2374aae401a1/lib/model/content.dart
+++
@@ -1,8 +0,0 @@
-
-class Content {
-  String? title;
-  String? status;
-  String? url;
-  bool? interrest=false;
-  Content({this.title,this.url,this.status,this.interrest});
-}
